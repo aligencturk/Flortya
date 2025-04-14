a042a455cfbb93a5cf8c08d7e4b390c18a506b60
aligencturk <alitalipgencturk@hotmail.com>
2025-04-14 04:52:24 +0300

    Firebase bağımlılıkları eklendi ve Google Services entegrasyonu sağlandı. build.gradle.kts dosyasında gerekli yapılandırmalar güncellendi.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,14 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    
+    // Firebase ürünleri için dependency ekleyin
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 flutter {
